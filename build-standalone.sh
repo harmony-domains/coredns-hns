68539cf91c783f6c9845e467c1d66a56a4ff004a
John Whitton <john@johnwhitton.com>
2023-01-22 11:30:25 -0800

    Update coredns build to use latest version

--- build-standalone.sh
+++ build-standalone.sh
@@ -4,13 +4,14 @@ set -e
 SRCDIR=`pwd`
 BUILDDIR=`pwd`/build
 
+rm -rf ${BUILDDIR}
 mkdir -p ${BUILDDIR} 2>/dev/null
 cd ${BUILDDIR}
 echo "Cloning coredns repo..."
 git clone https://github.com/coredns/coredns.git
 
 cd coredns
-git checkout v1.8.3
+git checkout v1.10.0
 
 echo "Patching plugin config..."
 ed plugin.cfg <<EOED
@@ -31,13 +32,13 @@ replace github.com/jw-1ns/coredns-1ns => ../..
 /^)
 -1
 a
-	github.com/jw-1ns/coredns-1ns v0.0.1
+	github.com/jw-1ns/coredns-1ns v0.1.0
 .
 w
 q
 EOED
 
-go get github.com/jw-1ns/coredns-1ns@v0.0.1
+go get github.com/jw-1ns/coredns-1ns@v0.1.0
 go get
 go mod download
 
